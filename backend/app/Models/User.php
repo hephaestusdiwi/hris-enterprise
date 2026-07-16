3593f370c5b48a2dd692b65723e447cdf15e7f7c
hephaestusdiwi <ilhadiwibusiness@gmail.com>
2026-07-16 13:57:20 +0700

    feat(step-16): install Spatie Laravel Permission
    - composer require spatie/laravel-permission
    - publish config/permission.php dan migration create_permission_tables
    - migrate: tabel roles, permissions, model_has_roles, model_has_permissions,
      role_has_permissions berhasil dibuat
    - HasRoles trait ditambahkan ke User model
    - diverifikasi: create role+permission, assign ke user, hasRole() dan can()
      keduanya return true

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -7,12 +7,13 @@ use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, HasRoles, Notifiable;
 
     /**
      * The attributes that are mass assignable.
